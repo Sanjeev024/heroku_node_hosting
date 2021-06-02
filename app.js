1f99c5785afd8a9020721311599732d2a1939fc5
Sanjeev Dubey <sanjeevdubey34@gmail.com>
2021-06-02 18:15:41 +0530

    fixed error

--- app.js
+++ app.js
@@ -1,27 +1,18 @@
+const host = "0.0.0.0";
+const port = process.env.PORT || 300;
 const express = require("express");
-const dotenv = require("dotenv");
-const mongoose = require("mongoose");
+
 const cors = require("cors");
 const app = express();
 app.use(cors());
 
-var session = require("express-session");
-app.use(
-  session({
-    secret: "1234567890QWERT",
-    resave: false,
-    saveUninitialized: true,
-    cookie: { maxAge: 60000 },
-  })
-);
-
 const host = "0.0.0.0";
 const port = process.env.PORT || 300;
 require("./db/conn");
-const USER = require("./model/userSchema");
+
 app.use(express.json());
 app.use(require("./routes/vendorAPI"));
 
-app.listen(port, host, () => {
+app.listen(port, () => {
   console.log(`Server app listening at http://localhost:${port}`);
 });
