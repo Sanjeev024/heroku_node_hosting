4ef01a85bf5eeaae5572521fb4f4bc2dcdb17a83
Sanjeev Dubey <sanjeevdubey34@gmail.com>
2021-06-02 18:20:39 +0530

    fixed error

--- app.js
+++ app.js
@@ -6,8 +6,8 @@ const cors = require("cors");
 const app = express();
 app.use(cors());
 
-const host = "0.0.0.0";
-const port = process.env.PORT || 300;
+// const host = "0.0.0.0";
+// const port = process.env.PORT || 300;
 require("./db/conn");
 
 app.use(express.json());
